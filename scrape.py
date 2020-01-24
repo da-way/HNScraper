d65b787fb177953a67f93bacf25fa84977eb0f3b
dave1089 <dave1089@live.com>
2020-01-24 13:14:21 -0800

    No Points Solution

--- scrape.py
+++ scrape.py
@@ -2,12 +2,15 @@ import requests as req
 from bs4 import BeautifulSoup as bs
 
 # Setup
-res = req.get('https://news.ycombinator.com/')
+res = req.get('https://news.ycombinator.com/news')
 soup = bs(res.text, 'html.parser')
-# links = soup.select('.storylink')
-# {title: "", link:"", score:" points"}  --
-# final = [l.getText() for l in links]
-# print()
-def create_custom_hn(soup):
-    pass
+links = soup.select('.storylink')
+st = soup.select('.subtext')
+for s in st:
+    ws = s.text.index(" ")
+    new = int(s.text[1:ws]) if s.find('span', class_="score") else "NONE"
+    print(new)
+    # new.append(s.text[1:ws] if s.get_text())
 
+# print(f'Links has {len(links)} & Subtext has {len(st)}')
+# print(new)
