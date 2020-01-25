69824e4d7768a84ada63498f9088419c4f0bb475
dave1089 <dave1089@live.com>
2020-01-25 14:16:45 -0800

    functionalized

--- scrape.py
+++ scrape.py
@@ -1,17 +1,21 @@
 import requests as req
 from bs4 import BeautifulSoup as bs
 
-# Setup
-res = req.get('https://news.ycombinator.com/news')
-soup = bs(res.text, 'html.parser')
-links_parent = soup.select('.storylink')
-points_parent = soup.select('.subtext')
+
+def data_fetch(base, page):
+    url = base + '?p=' + page
+    res = req.get(url)
+    soup = bs(res.text, 'html.parser')
+    links_parent = soup.select('.storylink')
+    points_parent = soup.select('.subtext')
+    print(create_custom_hn(links_parent, points_parent))
 
 
 def manipulation(nl):
     sort_list = sorted(nl, key=lambda k: int(k['points'][:k['points'].index(" ")]), reverse=True)
     filter_list = list(filter(lambda f: int(f['points'][:f['points'].index(" ")]) > 100, sort_list))
     return filter_list
+
 
 def points_list(pl):
     new_pl = []
@@ -37,5 +41,7 @@ def create_custom_hn(lp,pp):
     return manipulation(custom_hn)
 
 
-print(create_custom_hn(links_parent, points_parent))
+data_fetch('https://news.ycombinator.com/news', "1")
+
+
 
